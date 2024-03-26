1df189a6c433f099b5027944c08df9e82ca1fb0b
Andrew2401 <andrewshakin8@gmail.com>
2024-03-26 09:33:04 +0530

    edited Operator files

--- operators.java
+++
@@ -1,9 +0,0 @@
-public class operators {
-    public static void main(String[] args){
-        int a = 4;
-        int b = 5;
-        boolean result = a<b;
-        System.out.println(result);
-    }
-}
-
