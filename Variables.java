a9dac366da80fd9edca2f98fae0f5b0e4b308d90
Andrew2401 <andrewshakin8@gmail.com>
2024-03-26 09:03:51 +0530

    Variables file added

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args){
+        int num1 = 3;
+        int num2 = 1;
+        int result = num1 + num2;
+        System.out.println(result);
+        int newresult = result +1;
+        System.out.println(newresult);
+    }
+}
+
