e856121bf1cbb184664a792f3a102910a96d12b5
Andrew2401 <andrewshakin8@gmail.com>
2024-03-26 09:02:47 +0530

    Sample.java

---
+++ Sample.java
@@ -0,0 +1,20 @@
+public class Sample{
+    public static void main(String[] args){
+        System.out.println("hello");
+        System.out.println("New");
+        System.out.print("Project");
+        System.out.println(56);
+        System.out.println(3+5);
+
+            //This is comment
+            /* This is Multi 
+            Line comment */
+
+            //String -- "Hello"  Double quotes
+            //int -- stores integer values without decimal (123 or -123)
+            //float --stores floating point numbers with decimals (12.3 , 5.6 )
+            //char --   stores single characters 'a' 'b' 'c' 'd'
+            //boolean -- stores values with true or false
+    }
+} 
+
